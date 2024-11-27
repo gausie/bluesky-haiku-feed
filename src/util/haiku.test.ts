6beafc59e7857001934d1be6c904ad57c77d010b
Sam Gaus <sam@gaus.co.uk>
2024-11-27 12:05:07 +0000

    Test in CI

--- src/util/haiku.test.ts
+++ src/util/haiku.test.ts
@@ -7,3 +7,9 @@ test.each([
 ])('correctly identifies "%s" as a haiku', (text) => {
   expect(isHaiku(text)).toBe(true);
 });
+
+test.each([
+  "Ten years in and we bone like we're cheating on each other WITH each other. A decade-plus and her clit/brown/taint-area still pOwns my dick.",
+])('correctly identifies "%s" as not a haiku', (text) => {
+  expect(isHaiku(text)).toBe(false);
+});
